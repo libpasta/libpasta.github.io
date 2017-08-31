5f7d9ad3f94ca4c2fbfe8e3ca5a2605c1058745b
Sam Scott <sam.scott89@gmail.com>
2017-08-31 14:21:43 -0400

    rebuilding site Thu 31 Aug 14:21:43 EDT 2017

--- doc/implementors/rand/trait.SeedableRng.js
+++ doc/implementors/rand/trait.SeedableRng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libpasta"] = [];
+implementors["libpasta"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/rand/0.3/rand/trait.SeedableRng.html\" title=\"trait rand::SeedableRng\">SeedableRng</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">&amp;'a [</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">]</a>&gt; for Fortuna",];
 implementors["serde_mcf"] = [];
 
             if (window.register_implementors) {
--- doc/implementors/std/io/trait.BufRead.js
+++ doc/implementors/std/io/trait.BufRead.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libpasta"] = [];
+implementors["libpasta"] = ["impl&lt;L, R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a> for <a class=\"enum\" href=\"https://docs.rs/either/1/either/enum.Either.html\" title=\"enum either::Either\">Either</a>&lt;L, R&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;L: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;R: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a>,&nbsp;</span>",];
 implementors["serde_mcf"] = [];
 
             if (window.register_implementors) {
